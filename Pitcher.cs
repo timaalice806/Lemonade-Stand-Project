777c39823ec45bb1c65952864982b65dad910366
unknown <hjimmy0803@gmail.com>
2020-01-16 13:11:57 -0800

    Added member variables to Recipe class'

--- Pitcher.cs
+++ Pitcher.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Lemonade_Stand
 {
-    class Pitcher
+    public class Pitcher
     {
     }
 }
--- Weather.cs
+++ Weather.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Lemonade_Stand
 {
-    class Weather
+    public class Weather
     {
         //VARIABLES (WEATHER HAS A..)
         public 
