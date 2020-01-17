32d7a5b9cd524ba2205b30f9573ab463478847be
fatima <tima.alice806@gmail.com>
2020-01-17 12:41:22 -0600

    added random ability to weather, day, and customer classes

--- Customer.cs
+++ Customer.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Lemonade_Stand
 {
-    class Customer
+    public class Customer
     {
+        public int name;
+        public void initCustomers()
+        {
+            List<string> names = new List<string>() { "Annalee", "Kendra", "Coy", "Cecily", "Russell", "Tom", "Chris", "Donny", "Gerry", "Berniece", "Zeph", "Serrena", "Essie", "Gordie", "Kent", "Lovell", "Cy", "Merrick", "Reuben", "Judd" };
+
+            Random random = new Random();
+            name = random.Next(21);
+
+            for (int i = 0; i < name; i++)
+            {
+                Console.WriteLine($"customer: {names[name]}");    
+            }
+            Console.ReadLine();
+        }
     }
 }
